242b42bf4ead058aaea7a7fdf9e7af2050a331a5
akoenig-rgbg <andreas.koenig@st.oth-regensburg.de>
2017-01-11 15:50:18 +0100

    Design upload image section

---
+++ sharkz/src/main/java/de/othr/sw/sharkz/util/ImageUploadServlet.java
@@ -0,0 +1,54 @@
+package de.othr.sw.sharkz.util;
+
+import de.othr.sw.sharkz.model.InsertionPublishmentModel;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.jmimemagic.Magic;
+import net.sf.jmimemagic.MagicException;
+import net.sf.jmimemagic.MagicMatch;
+import net.sf.jmimemagic.MagicMatchNotFoundException;
+import net.sf.jmimemagic.MagicParseException;
+
+@WebServlet("/imageUpload")
+public class ImageUploadServlet extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        InsertionPublishmentModel publModel = (InsertionPublishmentModel) request.getSession().getAttribute("insertionPublishment");
+        
+        int image_id = Integer.parseInt(request.getParameter("image_id"));
+        
+        byte[] bytes = publModel.getInsertion().getImages().get(image_id);
+        
+        String mimeType;
+        
+        try {
+            MagicMatch match = Magic.getMagicMatch(bytes);
+            mimeType = match.getMimeType();
+        } catch (MagicException | MagicMatchNotFoundException
+                | MagicParseException ex) {
+            mimeType = "image/png";
+        }
+        
+        response.setContentType(mimeType);
+        OutputStream out = response.getOutputStream();
+        out.write(bytes);
+    }
+}
